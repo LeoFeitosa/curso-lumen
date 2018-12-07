10f76ac9b4151c15bab6f006d3eb92c114da128a
Leo <leonardo.feitosa@universidadebrasil.edu.br>
2018-12-07 11:09:45 -0200

    Crianco Model

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->group(["prefix" => "/api"], function() use ($router){
+    $router->get("/car", "CarsController@getAll");
+    $router->get("/car/{id}", "CarsController@get");
+    $router->post("/car", "CarsController@store");
+    $router->put("/car/{id}", "CarsController@update");
+    $router->delete("/car/{id}", "CarsController@destroy");
+});
